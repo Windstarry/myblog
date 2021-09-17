6a1e8d81a9efd548780e8677cccd8c17c5bc2100
Windstarry <13782656673@163.com>
2021-09-17 12:00:41 +0800

    增加帖子模型

--- app/models.py
+++ app/models.py
@@ -8,10 +8,23 @@
 '''
 # here put the import lib
 from app import db
+from datetime import datetime
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     def __repr__(self):
 	    return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    #user_id字段已作为一个外键初始化了，这表明它引用了users表中的id值
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __repr__(self):
+        return '<Post {}'.format(self.body)
--- manage.py
+++ manage.py
@@ -7,10 +7,22 @@
 @Version :   1.0
 '''
 # here put the import lib
-from app import app
+from app import app, db
 from flask_script import Manager
+from app.models import User, Post
 
 manager = Manager(app)
+
+'''
+app.shell_context_processor装饰器注册了一个作为shell 上下文功能的函数,
+当运行flask shell命令时，它将调用此函数并在shell会话中注册它返回的项,
+函数返回字典而不是列表的原因是：
+对于每个项目，我们还须提供一个名称，在这个名称下，它将在shell中被引用，由字典的键给出。
+'''
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 
 if __name__ == '__main__':
