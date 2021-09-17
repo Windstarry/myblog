f062d45f613e1f9b65291c28ca05d43de0f691f3
Windstarry <13782656673@163.com>
2021-09-17 18:03:05 +0800

    增加登录、退出功能

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(config['development'])
@@ -18,5 +19,7 @@ app.config.from_object(config['development'])
 db = SQLAlchemy(app)
 #迁移引擎对象
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 #导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
 from app import views,models
--- app/views.py
+++ app/views.py
@@ -9,13 +9,16 @@
 # here put the import lib
 from app import app
 from flask import render_template, flash, redirect, url_for
+from flask import request
 from app.forms import LoginForm
+from flask_login import current_user, login_user,logout_user,login_required
+from werkzeug.urls import url_parse
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    #用户
-    user = {'username':'Windstarry'}
     #创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
     posts = [
         {
@@ -27,13 +30,37 @@ def index():
         'body':'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {},remember_me={}'.format(form.username.data,form.remember_me.data))
-		return redirect(url_for('index'))
-	return render_template('login.html', title='登录', form=form)
+    '''
+    current_user变量来自Flask-Login,可在处理过程中随时使用，以表示请求客户端的用户对象。
+    此变量的值可以是数据库中的用户的对象Flask-Login通过上述提供的用户加载器回调读取的,
+    如果用户尚未登录，则可以是特殊的匿名用户对象
+    is_authenticated可以方便地检查用户是否登录,当用户已经登录时，只需重定向到/index页面
+    '''
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+		#重定向到 next 页面
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='登录', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
