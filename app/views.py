f74a9e1fa6a7ae64c0a1714b108b098c7c97c829
Windstarry <13782656673@163.com>
2021-09-19 21:17:34 +0800

    增加帖子分页功能

--- app/views.py
+++ app/views.py
@@ -28,16 +28,22 @@ def index():
         db.session.add(post)
         db.session.commit()
         flash('你的记录已发表')
-        return redirect(url_for('index'))   
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='主页',form=form, posts=posts)
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='主页',form=form, posts=posts.items,next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items,next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -93,6 +99,13 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     if user is None:
         abort(404)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
+    
+    
     posts = user.followed_posts().all()
     return render_template('user.html', user=user, posts=posts)
 
